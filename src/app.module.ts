c6aa2bab43210e28e01bb75706ccd72cd2d1c731
victorlhp <victor270196@gmail.com>
2024-04-15 15:42:09 -0300

    modulo do typeorm e banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PerguntasModule } from './perguntas.module'; // Importe o módulo de perguntas
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pergunta } from '././pergunta.entity'; // Importe a entidade de perguntas
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-co1gfq7109ks738go3sg-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 'banco_teste_0s9c_user',
+      password: 'NWmQnVAvRKtXLVUj0Hkd02QCAAX9l9Iw',
+      database: 'banco_teste_0s9c',
+      entities: [Pergunta], // Use a entidade Pergunta
+      synchronize: true,
+      ssl: true,
+    }),
+    PerguntasModule, // Use o módulo de perguntas
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
